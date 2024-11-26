4379780008a3314a5b23e2c9942d540efe2606eb
ThiagoTVarella <thiago_tv2@hotmail.com>
2024-11-26 15:03:27 -0500

    updatingproblems

--- 528.py
+++ 528.py
@@ -1,26 +1,64 @@
 import random
 import bisect
 
+# [2,3]
+# [0,2,5]
+
+# [1,2,3,4,5]
+
 class Solution:
     def __init__(self, w: List[int]):
-        self.prefix = []
-        prefix_sum = 0
+        self.prefix_sum = [0]
         for weight in w:
-            prefix_sum += weight
-            self.prefix.append(prefix_sum)
+            self.prefix_sum.append(self.prefix_sum[-1]+weight)
+        self.max = self.prefix_sum[-1]
 
     def pickIndex(self) -> int:
-        target = self.prefix[-1] * random.random()
-        # target = random.randint(1, self.prefix_sums[-1])
-        # run a binary search to find the target zone
+        x = random.randint(1,self.max)
+        # ind = bisect.bisect_left(self.prefix_sum,x)
+        # return ind-1
+        
+        lo = 0
+        hi = len(self.prefix_sum)-1
 
-        # low, high = 0, len(self.prefix)
-        # while low < high:
-        #     mid = low + (high - low) // 2
-        #     if target > self.prefix[mid]:
-        #         low = mid + 1
-        #     else:
-        #         high = mid
+        while lo < hi:
+            mi = lo + (hi-lo)//2
 
-        low = bisect_left(self.prefix, target)
-        return low
+            if self.prefix_sum[mi] == x:
+                return mi-1
+            
+            if self.prefix_sum[mi] < x:
+                lo = mi+1
+            else:
+                hi = mi
+        
+        return lo-1
+
+    # lo hi
+    #    x
+    # [0,1,4,5]
+    # x = [1,2,3,4,5]
+    # 
+
+    #     self.cumsum = [0]
+    #     for weight in enumerate(w):
+    #         self.cumsum.append(self.cumsum[-1] + weight)
+    #     self.max = self.cumsum[-1]
+
+    #     # 0 3 17 18 25
+
+    # def pickIndex(self) -> int:
+    #     r = random.random()*self.max
+    #     # pos = bisect.bisect_left(self.cumsum,r)-1
+        
+    #     left = 0
+    #     right = len(self.cumsum)-1
+    #     while left < right:
+    #         mid = left + (right-left)//2 # mid is 0
+            
+    #         if self.cumsum[mid] < r:
+    #             left = mid+1
+    #         elif self.cumsum[mid] >= r:
+    #             right = mid # right is 1, left is 0
+
+    #     return left-1
--- 88.py
+++ 88.py
@@ -1,39 +1,79 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        
-        if n == 0:
-            pass
-        if m == 0:
-            for i in range(n):
-                nums1[i] = nums2[i]
-        else:             
-            # Create pointers i_m and i_n as the last non-decreasing element of nums1 and nums2
-            i_m = m-1
-            i_n = n-1
+
+        counter_1 = m-1
+        counter_2 = n-1
+        pointer = m+n-1
+
+        while pointer >= 0:
+
+            if counter_1 >= 0 and counter_2 >= 0:
+                if nums1[counter_1] >= nums2[counter_2]:
+                    nums1[pointer] = nums1[counter_1]
+                    counter_1 -= 1
+                else:
+                    nums1[pointer] = nums2[counter_2]
+                    counter_2 -= 1
+
+            elif counter_1 < 0 and counter_2 >= 0:
+                nums1[pointer] = nums2[counter_2]
+                counter_2 -= 1
             
-            # Iterate over final version of nums1 with counter
-            counter = m+n-1
+            pointer -= 1
+            
 
-            print(nums1, i_m, counter)
-            print(nums2, i_n)
+     
 
-            while counter >= 0:
-                # Compare elements im and in and store highest at position counter of nums1
-                # Iterate pointer im or in depending on the highest element             
-                
-                if i_m >= 0 and i_n >= 0:
-                    if nums1[i_m] >= nums2[i_n]:
-                        nums1[counter] = nums1[i_m]
-                        i_m -= 1
-                    else:
-                        nums1[counter] = nums2[i_n]
-                        i_n -= 1
-                elif i_m < 0 and i_n >= 0:
-                    nums1[counter] = nums2[i_n]
-                    i_n -= 1
-                elif i_n < 0:
-                    break
-                counter -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # pt1 = m-1
+        # pt2 = n-1
+        # pt = m+n-1
+
+        # while pt >= 0:
+            
+        #     if pt1 < 0:
+        #         nums1[pt] = nums2[pt2]
+        #         pt2 -= 1
+        #         pt -= 1
+
+        #     elif pt2 < 0:
+        #         nums1[pt] = nums1[pt1]
+        #         pt1 -= 1
+        #         pt -= 1
+
+        #     elif nums1[pt1] >= nums2[pt2]:
+        #         nums1[pt] = nums1[pt1]
+        #         pt1 -= 1
+        #         pt -= 1
+
+        #     else:
+        #         nums1[pt] = nums2[pt2]
+        #         pt2 -= 1
+        #         pt -= 1
+
+
+
+        # #[1,0] [1]
+
+
+
+
